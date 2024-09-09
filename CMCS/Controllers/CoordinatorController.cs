3157514ce90a1ff0460a4ae7c941e02d949fbb75
Alton Muganda <ST10263456@vcconnect.edu.za>
2024-09-09 19:03:02 +0200

    innit Commit

---
+++ CMCS/Controllers/CoordinatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMCS.Controllers
+{
+    public class CoordinatorController : Controller
+    {
+        public IActionResult VerifyClaims()
+        {
+            return View();
+        }
+    }
+}
---
+++ CMCS/Controllers/LectureController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMCS.Controllers
+{
+    public class LectureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UploadDocuments()
+        {
+            return View();
+        }
+
+        public ActionResult TrackClaim()
+        {
+            return View();
+        }
+
+    }
+}
